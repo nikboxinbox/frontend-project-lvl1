ffb642aa079afe898e4dca6d0f2bcc5be09bd46b
Nik Nik <nikbox88@gmail.com>
2021-11-16 13:15:24 +0300

    feat: step three

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+export const toGreet = () => {
+
+const name = readlineSync.question('May I have your name? ');
+
+console.log('Hello, ' + name + '!')}
+
+
+
+
